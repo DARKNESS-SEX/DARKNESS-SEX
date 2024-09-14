9e9487d5c62211ef682b0c4ed08f7f9b4f82a111
Greatnishioka <114734251+Greatnishioka@users.noreply.github.com>
2024-09-15 02:52:36 +0900

    components fix

---
+++ src/nextjs/pages/components/hukidashi.js
@@ -0,0 +1,7 @@
+export default function Hukidashi(props) {
+  return (
+    <svg width="413" height="281" viewBox="0 0 413 281" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M349.03 33L317.208 0.055542L320.766 33H51H33H30.1843C-9.46453 71.7022 -10.654 244.335 30.1843 281H33H51H362H380H382.816C423.654 244.335 422.465 71.7022 382.816 33H380H362H349.03Z" fill="white"/>
+    </svg>
+  );
+}
--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
 import packageJson from "../package.json";
+import Image from 'next/image'
+import Hukidashi from './components/hukidashi';
+import Nav_bar from './components/nav_bar';
+
+//import "../styles/output.css";
 
 export default function Home(props) {
   /** You can access to liff and liffError object through the props.
@@ -9,65 +14,14 @@ export default function Home(props) {
    *  Learn more about LIFF API documentation (https://developers.line.biz/en/reference/liff)
    **/
   return (
-    <div>
+    <main className="">
       <Head>
         <title>LIFF Starter</title>
       </Head>
-      <div className="home">
-        <h1 className="text-sky-500">hogehoge</h1>
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">
-            LIFF Starter
-          </span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--primary">
-            {packageJson.version}
-          </span>
-          <a
-            href="https://github.com/line/line-liff-v2-starter"
-            target="_blank"
-            rel="noreferrer"
-            className="home__badges__badge badge--secondary"
-          >
-            GitHub
-          </a>
-        </div>
-        <div className="home__buttons">
-          <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--primary"
-          >
-            LIFF Documentation
-          </a>
-          <a
-            href="https://liff-playground.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--tertiary"
-          >
-            LIFF Playground
-          </a>
-          <a
-            href="https://developers.line.biz/console/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--secondary"
-          >
-            LINE Developers Console
-          </a>
-        </div>
+      <div className="w-[375px] ">
+        hogehoge
+        <Nav_bar />
       </div>
-    </div>
+    </main>
   );
 }
