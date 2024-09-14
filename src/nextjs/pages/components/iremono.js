2d001d3eb9e7fdc9eb70a59ef54e245bcd94ebe7
Makimaki1304 <23e1304@andrew.ac.jp>
2024-09-15 04:00:57 +0900

    add components

---
+++ src/nextjs/pages/components/iremono.js
@@ -0,0 +1,8 @@
+export default function Iremono(props) {
+    return (
+        <div>
+            <div className="text-base">好きなもの</div>
+            <input type="text" className="w-4/5 rounded border-slate-400 text-base border-solid border" placeholder="(例) ゲーム"/>
+        </div>
+    );
+  }
---
+++ src/nextjs/pages/register/index.js
@@ -0,0 +1,13 @@
+import Iremono from ".././components/iremono";
+
+export default function Home(props) {
+    /** You can access to liff and liffError object through the props.
+     *  const { liff, liffError } = props;
+     *  console.log(liff.getVersion());
+     *
+     *  Learn more about LIFF API documentation (https://developers.line.biz/en/reference/liff)
+     **/
+    return (
+        <Iremono></Iremono>
+    );
+}
