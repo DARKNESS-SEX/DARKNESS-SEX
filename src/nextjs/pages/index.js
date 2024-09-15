6e15cf7c6a0aa1d0aa2ff1123ee75491c34c7fa1
agoetc <tkgw666private@gmail.com>
2024-09-15 14:03:40 +0900

    add modal

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
-import packageJson from "../package.json";
-import Image from 'next/image'
 import Modal from './components/modal';
 import Nav_bar from './components/nav_bar';
 import Schedule_card from './components/schedule_card'
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 //import "../styles/output.css";
@@ -21,7 +19,9 @@ async function getSchedules() {
 
 export default function Home(props) {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
 
   useEffect(() => {
     getSchedules().then(result => {setData(result)
@@ -33,15 +33,23 @@ export default function Home(props) {
       <Head>
         <title>LIFF Starter</title>
       </Head>
-      <Modal className="" />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onAccept={() => {
+          console.log('Accepted');
+
+          setIsModalOpen(false);
+        }}
+      />
       <div className="w-[375px] h-[100vh] relative top-5 block">
         <h1 className="text-center text-[36px]">スケジュール</h1>
         <div className="flex justify-center items-center flex-col pb-[120px]">
         {data.length > 0 ? data.map((schedule, index) => (
-            <Schedule_card key={index} $time={`${schedule.start_date_time}~${schedule.end_date_time}`} $title={schedule.title} $contents={schedule.contents} />
+            <Schedule_card key={index} $time={`${schedule.start_date_time}~${schedule.end_date_time}`} $title={schedule.title} $contents={schedule.contents} $onClick={() => setIsModalOpen(true)} />
           )) : "Loading..."}
         </div>
-        
+
         <Nav_bar />
       </div>
     </main>
