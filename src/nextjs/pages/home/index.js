9a213b90ead6bbc647ea515821a9e5d76911c637
Makimaki1304 <23e1304@andrew.ac.jp>
2024-09-15 13:38:23 +0900

    home

---
+++ src/nextjs/pages/home/index.js
@@ -0,0 +1,23 @@
+import Iremono from ".././components/iremono";
+import Button from "../components/button";
+import Eraberu from "../components/eraberu";
+import Nav_bar from "../components/nav_bar";
+import Image from "next/dist/client/image";
+import localImages from "../../public/dorara.png";
+import BgImg from "../../public/blu.png";
+
+export default function Home(props) {
+    /** You can access to liff and liffError object through the props.
+     *  const { liff, liffError } = props;
+     *  console.log(liff.getVersion());
+     *
+     *  Learn more about LIFF API documentation (https://developers.line.biz/en/reference/liff)
+     **/
+    return (
+        <div className="relative h-screen w-screen">
+            <Image src={localImages} className="relative z-10 bg-cover" />
+            <Image src={BgImg} layout="fill" objectFit="cover"/>
+            <Nav_bar />
+        </div>
+    );
+}
